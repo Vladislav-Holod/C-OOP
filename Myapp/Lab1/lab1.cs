97775a6d8fbc6dfc56c7000cc8596af224b35e8f
Vladislav-Holod <vladisjack22@gmail.com>
2026-03-30 22:26:53 +0300

    lab

---
+++ Myapp/Lab1/lab1.cs
@@ -0,0 +1,10 @@
+namespace Lab1
+{
+    public class Lab1
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Это лабораторная 1");
+        }
+    }
+}
--- Myapp/Program.cs
+++ Myapp/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Myapp.Lab1;
+// using Myapp.Lab2;
+// using Myapp.Lab3;
+// using Myapp.Lab4;
+// using Myapp.Lab5;
+// using Myapp.Lab6;
+Lab1.Run();
